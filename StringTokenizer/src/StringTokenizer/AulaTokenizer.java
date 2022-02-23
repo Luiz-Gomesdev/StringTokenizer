896141291573d07f806c5e60b52d776188df8a21
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-02-23 07:35:24 -0300

    StringTokenizer

---
+++ StringTokenizer/src/StringTokenizer/AulaTokenizer.java
@@ -0,0 +1,19 @@
+package StringTokenizer;
+
+import java.util.StringTokenizer;
+
+public class AulaTokenizer {
+
+	public static void main(String[] args) {
+
+		String doArquivo = "1;Antônio;30;";
+
+		StringTokenizer st = new StringTokenizer(doArquivo, ";");
+
+		while (st.hasMoreTokens()){
+			System.out.println(st.nextToken());
+		}
+
+	}
+
+}
